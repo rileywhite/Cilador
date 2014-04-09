724942068bd24f7459e4403c3f82dead893ecfcd
Riley White <rileywhite@gmail.com>
2014-04-09 15:39:07 -0500

    Working on NuGet packaging

--- src/Bix.Mixers/Fody.Tests/Properties/AssemblyInfo.cs
+++ src/Bix.Mixers/Fody.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bix.Mixers.Fody.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Riley White 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
