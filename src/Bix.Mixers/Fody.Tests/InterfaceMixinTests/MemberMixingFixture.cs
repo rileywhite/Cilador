545322e08a44a1cea6fac9ed5e5f4071428ec3f3
Riley White <rileywhite@gmail.com>
2014-04-20 19:06:07 -0500

    Test data and such for generics

--- src/Bix.Mixers/Fody.Tests/InterfaceMixinTests/MemberMixingFixture.cs
+++ src/Bix.Mixers/Fody.Tests/InterfaceMixinTests/MemberMixingFixture.cs
@@ -183,7 +183,7 @@ namespace Bix.Mixers.Fody.Tests.InterfaceMixinTests
             var assembly = ModuleWeaverHelper.WeaveAndLoadTestTarget(config);
             var targetType = assembly.GetType(typeof(Bix.Mixers.Fody.TestMixinTargets.EmptyInterfaceTarget).FullName);
             Assert.That(typeof(IEmptyInterface).IsAssignableFrom(targetType));
-            targetType.ValidateMemberCountsAre(1, 12, 0, 0, 0, 0);
+            targetType.ValidateMemberCountsAre(1, 14, 0, 0, 0, 0);
             Assert.That(targetType.GetConstructor(new Type[0]) != null, "Lost existing default constructor");
 
             targetType.ValidateMemberSources(typeof(GenericsMixin));
