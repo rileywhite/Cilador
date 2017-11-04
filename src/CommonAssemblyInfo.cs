203251ce5b495320e11b5606090642c803c68e8f
Riley White <rileywhite@gmail.com>
2017-11-04 11:57:33 -0500

    Update copyrights

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿/***************************************************************************/
-// Copyright 2013-2016 Riley White
+// Copyright 2013-2017 Riley White
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Riley White")]
 [assembly: AssemblyProduct("Cilador")]
-[assembly: AssemblyCopyright("Copyright © Riley White 2013-2016")]
+[assembly: AssemblyCopyright("Copyright © Riley White 2013-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion(CommonAssemblyInfo.Version)]
 [assembly: AssemblyFileVersion(CommonAssemblyInfo.Version)]
